496c2e4798b0196cbb1f5ffafdbdb8e6e875f8ee
robson-sampaio <robson.sampaio@dellead.com>
2022-03-15 15:58:26 -0300

    Decreasing the number of shots for moviment

--- src/myrect.cpp
+++ src/myrect.cpp
@@ -7,26 +7,28 @@
 #include <QKeyEvent>
 
 QSet<int> key;
+int countRight = 0;
+int countLeft = 0;
 
 MyRect::MyRect(QGraphicsItem *parent): QGraphicsPixmapItem(parent){
     setPixmap(QPixmap("/home/robson/projetos/space_war/assets/images/spaceship_small.png"));    
 }
 
 void MyRect::keyReleaseEvent(QKeyEvent *event) {
-    key.remove(event->key());
-    qDebug() << key.size();        
+    key.remove(event->key());     
 }
 
 void MyRect::keyPressEvent(QKeyEvent *event) {    
     key.insert(event->key());
-    qDebug() << key.size();
-    
-    // while(key.size() > 0){
         if (key.contains(Qt::Key_Left)){
             if (key.contains(Qt::Key_Space)){
-                Bullet *bullet = new Bullet();
-                bullet->setPos(x() + 100/2, y());
-                scene()->addItem(bullet);
+                countLeft++;
+                if(countLeft > 5){
+                    Bullet *bullet = new Bullet();
+                    bullet->setPos(x() + 100/2, y());
+                    scene()->addItem(bullet);
+                    countLeft = 0;
+                }
             }            
             if (x()>0){
                 setPos(x()-10, y());
@@ -34,9 +36,13 @@ void MyRect::keyPressEvent(QKeyEvent *event) {
         }
         else if (key.contains(Qt::Key_Right)){
             if (key.contains(Qt::Key_Space)){
-                Bullet *bullet = new Bullet();
-                bullet->setPos(x() + 100/2, y());
-                scene()->addItem(bullet);
+                countRight++;
+                if(countRight > 5){
+                    Bullet *bullet = new Bullet();
+                    bullet->setPos(x() + 100/2, y());
+                    scene()->addItem(bullet);
+                    countRight = 0;
+                }
             }            
             if (x()>0){
                 setPos(x()+10, y());
